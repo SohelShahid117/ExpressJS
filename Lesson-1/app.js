d49ae6663b3c25547c180847c3feefb941e504f9
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-20 23:17:18 +0600

    tutorial 3 : http methods and postman done

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -1,4 +1,20 @@
+//express js bangla tutorial 3 : http methods and postman
 const express = require("express")
 const app = express()
+app.get("/",(req,res)=>{
+    res.send('i am a get request at home(/) route')
+})
+// app.get("/about",(req,res)=>{
+//     res.send('i am a get request at about route')
+// })
+app.post("/",(req,res)=>{
+    res.send('i am a post request at home route')
+})
+app.put("/",(req,res)=>{
+    res.send('i am a put request at home route')
+})
+app.get("/delete",(req,res)=>{
+    res.send('i am a get request at delete route')
+})
 
 module.exports = app
