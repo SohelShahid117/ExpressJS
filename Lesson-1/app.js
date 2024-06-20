48a8b5c509b6b051b6e9ab710d352917b3015d76
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-20 23:18:48 +0600

    add res.end() method

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -3,18 +3,22 @@ const express = require("express")
 const app = express()
 app.get("/",(req,res)=>{
     res.send('i am a get request at home(/) route')
+    res.end()
 })
 // app.get("/about",(req,res)=>{
 //     res.send('i am a get request at about route')
 // })
 app.post("/",(req,res)=>{
     res.send('i am a post request at home route')
+    res.end()
 })
 app.put("/",(req,res)=>{
     res.send('i am a put request at home route')
+    res.end()
 })
 app.get("/delete",(req,res)=>{
     res.send('i am a get request at delete route')
+    res.end()
 })
 
 module.exports = app
