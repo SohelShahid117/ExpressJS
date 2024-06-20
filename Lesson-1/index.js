a009b24e54bb5d7d67be40acd3e86f0e3c983615
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-20 23:04:08 +0600

    tutorial 2 : how to create express server done

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -1,2 +1,13 @@
 //express js bangla tutorial 2 : how to create express server
 //https://youtu.be/t9GVn5j1Hsw?si=jBiPGgPIXSa_0cvF
+//get,post,put,delete
+//read,create,update,delete
+
+const app = require('./app')
+
+const port = 3000
+const hostName = '127.0.0.1'
+
+app.listen(port,()=>{
+    console.log(`server is running at http://${hostName}:${port}`)
+})
