b73530027e7b311503afd18de153d0ef61ab6d08
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-21 14:03:22 +0600

    tutorial 4 : express js router and routes done

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -3,6 +3,9 @@
 const express = require("express")
 const app = express()
 
+const userRouter = require('./routes/user.route')
+app.use("/api/user",userRouter)
+
 /*
 app.get("/",(req,res)=>{
     res.send('<h1>i am a get request at home(/) route</h1>')
@@ -33,6 +36,34 @@ app.delete("/",(req,res)=>{
 })
 */
 
+app.use("/",(req,res)=>{
+    res.send('<h1>i am a get request at home(/) route</h1>')
+    res.end()
+})
+
+app.use("/about",(req,res)=>{
+    res.send('<h1>i am a get request at about route</h1>')
+})
+app.use("/register",(req,res)=>{
+    res.send('<h1>i am a get request at register route</h1>')
+})
+app.use("/login",(req,res)=>{
+    res.send('<h1>i am a get request at login route</h1>')
+})
+
+app.use("/",(req,res)=>{
+    res.send('<h1>i am a post request at home route</h1>')
+    res.end()
+})
+app.use("/",(req,res)=>{
+    res.send('<h1>i am a put request at home route</h1>')
+    res.end()
+})
+app.use("/",(req,res)=>{
+    res.send('<h1>i am a delete request at delete route</h1>')
+    res.end()
+})
+
 app.use((req,res)=>{
     res.send("<h1>404 !!! Page Not Found</h1>")
 })
@@ -40,42 +71,3 @@ app.use((req,res)=>{
 module.exports = app
 
 
-/*
-const express = require("express")
-const route = express.Router()
-
-
-route.get("/",(req,res)=>{
-    res.send('<h1>i am a get request at home(/) route</h1>')
-    res.end()
-})
-
-route.get("/about",(req,res)=>{
-    res.send('<h1>i am a get request at about route</h1>')
-})
-route.get("/register",(req,res)=>{
-    res.send('<h1>i am a get request at register route</h1>')
-})
-route.get("/login",(req,res)=>{
-    res.send('<h1>i am a get request at login route</h1>')
-})
-
-route.post("/",(req,res)=>{
-    res.send('<h1>i am a post request at home route</h1>')
-    res.end()
-})
-route.put("/",(req,res)=>{
-    res.send('<h1>i am a put request at home route</h1>')
-    res.end()
-})
-route.delete("/",(req,res)=>{
-    res.send('<h1>i am a delete request at delete route</h1>')
-    res.end()
-})
-route.use((req,res)=>{
-    res.send("<h1>404 !!! Page Not Found</h1>")
-})
-
-module.exports = app
-
-*/
--- Lesson-1/routes/user.route.js
+++ Lesson-1/routes/user.route.js
@@ -1,30 +1,37 @@
+//express js bangla tutorial 4 : express js router and routes
 const express = require("express")
 const router = express.Router()
 
-app.get("/",(req,res)=>{
+
+router.get("/",(req,res)=>{
     res.send('<h1>i am a get request at home(/) route</h1>')
     res.end()
 })
 
-app.get("/about",(req,res)=>{
+router.get("/about",(req,res)=>{
     res.send('<h1>i am a get request at about route</h1>')
 })
-app.get("/register",(req,res)=>{
+router.get("/register",(req,res)=>{
     res.send('<h1>i am a get request at register route</h1>')
 })
-app.get("/login",(req,res)=>{
+router.get("/login",(req,res)=>{
     res.send('<h1>i am a get request at login route</h1>')
 })
 
-app.post("/",(req,res)=>{
+router.post("/",(req,res)=>{
     res.send('<h1>i am a post request at home route</h1>')
     res.end()
 })
-app.put("/",(req,res)=>{
+router.put("/",(req,res)=>{
     res.send('<h1>i am a put request at home route</h1>')
     res.end()
 })
-app.delete("/",(req,res)=>{
+router.delete("/",(req,res)=>{
     res.send('<h1>i am a delete request at delete route</h1>')
     res.end()
 })
+router.use((req,res)=>{
+    res.send("<h1>404 !!! Page Not Found</h1>")
+})
+
+module.exports = router
